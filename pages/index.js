7705df4152727ea1d844beca85d04c9651f551be
styu12 <styu12@naver.com>
2022-07-01 16:48:08 +0900

    Deploy Test

--- pages/index.js
+++ pages/index.js
@@ -24,7 +24,7 @@ export default function Home({ allPostsData }) {
         <p>[Your Self Introduction]</p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+          <a href="https://nextjs.org/learn">our Next.js tutorialallalalalalalalalalalal</a>.)
         </p>
       </section>
 
